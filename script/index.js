38a59012f4fa74bc5cb5a854613e2f2840544831
Sanskar Diwedi <sanskaryeeha@gmail.com>
2023-10-02 23:17:53 +0530

    added a new file

---
+++ script/index.js
@@ -0,0 +1,27 @@
+// const url = document.getElementById('name').value;
+
+// function downloadvideo(){
+//     const VIDEO_URL = url;
+//     const FILENAME = video.mp4;
+//     fetch(VIDEO_URL)
+//         .then(function(response){
+//             return response.blob();
+//         })
+//         .then(function(blob){
+//             const url = window.URL.createObjectURL(blob);
+
+//             const downloadb = document.getElementById("butt");
+//             downloadb.href = url;
+//             downloadb.downloadvideo=filename;
+
+//             window.URL.revokeObjectURL(url);
+//         })
+//         .catch(function(error) {
+//             console.error("Error downloading video:", error);
+//         });
+    
+    
+// 
+
+
+
